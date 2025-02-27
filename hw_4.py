e6c03b448f9f7eb612838daa1050b3b8db5d5d1d
V1aD1slavee <vladboulderchannel@gmail.com>
2025-02-27 18:21:08 +0200

    added method study into class Student

--- hw_4.py
+++ hw_4.py
@@ -14,6 +14,9 @@ class Student(GeeksPeople):
         self.student_id = student_id
         self.group_where_study = group_where_study
 
+    def study(self):
+        print(f"Студент: {self.name}\nУчится в группе под названием: {self.group_where_study}")
+
 
 class Teacher(GeeksPeople):
 
@@ -24,8 +27,11 @@ class Teacher(GeeksPeople):
 
 
 class Admin(GeeksPeople):
-    def __init__(self, name, email, phone_number):
+    def __init__(self, name, email, phone_number, admin_id):
         GeeksPeople.__init__(self, name, email, phone_number)
+        self.admin_id = admin_id
 
 
 p1 = GeeksPeople("Vlad", "vladboulderchannel@gmail.com", "0505666038")
+st1 = Student("Владислав", "vladboulder@gmail.com", "+79775782690", 1, "25-1B")
+st1.study()
