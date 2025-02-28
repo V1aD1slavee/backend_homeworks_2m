1744d309e94eeba79feaaf99173a0015d85070d7
V1aD1slavee <vladboulderchannel@gmail.com>
2025-02-28 12:59:37 +0200

    code is finished

--- hw_4.py
+++ hw_4.py
@@ -19,14 +19,13 @@ class Student(GeeksPeople):
 
 
 class Teacher(GeeksPeople):
-
     def __init__(self, name, email, phone_number, teacher_id, group_where_teach):
         GeeksPeople.__init__(self, name, email, phone_number)
         self.teacher_id = teacher_id
         self.group_where_teach = group_where_teach
 
     def teach(self):
-        print(f"Преподователь: {self.name}\nПерподаёт в группе под названием: {self.group_where_study}")
+        print(f"Преподователь: {self.name}\nПерподаёт в группе под названием: {self.group_where_teach}")
 
 
 class Admin(GeeksPeople):
@@ -34,7 +33,44 @@ class Admin(GeeksPeople):
         GeeksPeople.__init__(self, name, email, phone_number)
         self.admin_id = admin_id
 
+    def create_group(self, name_group):
+        print(f"Новая группа создана : {name_group}")
 
-p1 = GeeksPeople("Vlad", "vladboulderchannel@gmail.com", "0505666038")
+class Mentor(Student, Teacher):
+    def __init__(
+        self,
+        name,
+        email,
+        phone_number,
+        student_id,
+        group_where_study,
+        teacher_id,
+        group_where_teach,
+    ):
+        Student.__init__(self, name, email, phone_number, student_id, group_where_study)
+        Teacher.__init__(self, name, email, phone_number, teacher_id, group_where_teach)
+
+
+# p1 = GeeksPeople("Vlad", "vladboulderchannel@gmail.com", "0505666038")
 st1 = Student("Владислав", "vladboulder@gmail.com", "+79775782690", 1, "25-1B")
+tch1 = Teacher("Владислав", "vlad@gmail.com", "0226606163", 2,"25-1B")
+admin1 = Admin("Vladislav", "vladboulderchannel@gmail.com", "0505666038", 3)
+ment1 = Mentor("Vladislav", "vladboulderchannel@gmail.com", "0505666038", 4, "25-1B", 5, "27-1B")
+
+print(st1)
 st1.study()
+print("--------------------------------")
+
+print(tch1)
+tch1.teach()
+print("--------------------------------")
+
+
+print(admin1)
+admin1.create_group("25-2B")
+print("--------------------------------")
+
+print(ment1)
+ment1.study()
+ment1.teach()
+print("--------------------------------")
